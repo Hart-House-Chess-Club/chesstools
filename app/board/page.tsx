ece946aab163f4935d9311a0bff54a6fa1984e4a
victor-zheng-codes <victork.zheng@mail.utoronto.ca>
2024-12-24 21:33:49 -0500

    cards for the page and play

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -1,13 +1,27 @@
 // import ChessBoardRandom from '@/components/ChessBoard';
 import BasicBoard from '@/components/BasicBoard';
+// import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 
 export default function ChessBoard() {
   return (
+    <Card className="w-full max-w-md mx-auto">
+      <CardHeader>
+        <CardTitle>Board to FEN</CardTitle>
+      </CardHeader>
+    <CardContent className="space-y-4">
     <div className="max-w-3xl mx-auto mt-8">
-      <h1 className="text-3xl font-bold mb-6 text-center">Modify the board</h1>
       <div className='max-w-0.5xl'>
         <BasicBoard/>
       </div>
     </div>
+    </CardContent>
+
+    <CardFooter>
+        {/* <Button onClick={handleFetchImage} disabled={loading} className="w-full">
+          {loading ? "Generating..." : "Generate Chess Board Image"}
+        </Button> */}
+      </CardFooter>
+    </Card>
   );
 }
--- app/play/page.tsx
+++ app/play/page.tsx
@@ -1,13 +1,26 @@
 import ChessBoardRandom from '@/components/ChessBoard';
 // import BasicBoard from '@/components/BasicBoard';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 
 export default function ChessBoard() {
   return (
+    <Card className="w-full max-w-md mx-auto">
+    <CardHeader>
+      <CardTitle>Play with a random chess board</CardTitle>
+    </CardHeader>
+  <CardContent className="space-y-4">
     <div className="max-w-3xl mx-auto mt-8">
-      <h1 className="text-3xl font-bold mb-6 text-center">Play with a random chess board</h1>
       <div className='max-w-2xl'>
         <ChessBoardRandom/>
       </div>
     </div>
+    </CardContent>
+
+    <CardFooter>
+        {/* <Button onClick={handleFetchImage} disabled={loading} className="w-full">
+          {loading ? "Generating..." : "Generate Chess Board Image"}
+        </Button> */}
+      </CardFooter>
+    </Card>
   );
 }
