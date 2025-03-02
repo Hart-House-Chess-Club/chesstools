24de51f5bc7a3c8f2e44b1ab010db3ff885d2381
victor-zheng-codes <victork.zheng@mail.utoronto.ca>
2025-03-02 16:29:43 -0500

    stockfish fixes

--- components/stockfish/engine.ts
+++ components/stockfish/engine.ts
@@ -7,7 +7,7 @@
  * Description of the universal chess interface (UCI)  https://gist.github.com/aliostad/f4470274f39d29b788c1b09519e67372/
  */
 
-const stockfish = new Worker("./stockfish.js");
+// const stockfish = new Worker("./stockfish.js");
 
 type EngineMessage = {
   /** stockfish engine message in UCI format*/
@@ -32,8 +32,7 @@ export default class Engine {
   isReady: boolean;
 
   constructor() {
-
-    this.stockfish = stockfish;
+    this.stockfish = new Worker("./stockfish.js");
     this.isReady = false;
     this.onMessage = (callback) => {
       this.stockfish.addEventListener("message", (e) => {
